f02d73fbeb39b4587f7f936a3e23efaf6d8466fd
Jamille Ni <jamilletni@gmail.com>
2023-02-02 18:06:56 -0500

    added the abilities to perform add, edit, complete functions in one line of command, see README file for more details

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-open("user_file.txt","a").close()       #create an .txt file if there isn't one already
+#create an .txt file if there isn't one already
+open("user_file.txt","a").close()
 with open("user_file.txt","r") as f:
     todos = f.readlines()
 
-def add():
-    todo = input ("Enter a to-do: ")+"\n"
+
+def add(user_input="add"):
+    if user_input=="add":
+        todo = input ("Enter a to-do: ")+"\n"
+    else:
+        todo = user_input[3:].strip()+"\n"
     todos.append(todo.capitalize())
+
 
 def show():
     print("---------------")
@@ -12,37 +18,57 @@ def show():
         print (f"{i+1} - {item}", end="")
     print("---------------")
 
-def edit():
-    num = int (input("Please enter the # to edit: "))
+
+def edit(user_input="edit"):
+    if user_input=="edit":
+        num = int (input("Please enter the # to edit: "))
+    else:
+        num = int (user_input[5])
     
-    if num > len(todos):      #if the entered # is larger than # of items on list, it will add a new item instead of edit the existing one
+    #if the entered # is larger than # of items on list, 
+    #it will add a new item instead of edit the existing one
+    if num > len(todos):
         add()
     else:
         num = num - 1
-        new_todo = input("Please enter the new to-do: ")+"\n"
-        todos[num] = new_todo
+        if len(user_input)==5:
+            new_todo = input("Please enter the new to-do: ")+"\n"
+        else:
+            new_todo = user_input[7:]+"\n"
 
-def complete():
-    num = int (input("Please enter the # you have completed: "))
+    todos[num] = new_todo
+
+
+def complete(user_input="complete"):
+    if user_input=="complete":
+        num = int (input("Please enter the # you have completed: "))
+    else:
+        num = int (user_input[9])
     num = num - 1
-    print(f"----'{todos.pop(num).strip()}' has been marked complete----")       #added .strip() to remove the \n
+    #added .strip() to remove the \n
+    print(f"----'{num+1} - {todos.pop(num).strip()}' has been marked complete----")
+
 
 def main():
     while True:
         user_input = input ("Type add, show, edit, complete or exit\n")
-        user_input = user_input.strip().lower()
+        user_input = user_input.strip()
+        if " " in user_input:
+            user_cmd = user_input[:user_input.find(" ")].lower()
+        else:
+            user_cmd = user_input.lower()
 
-        match user_input:
+        match user_cmd:
             case "add":
-                add()
+                add(user_input)
             case "show":
                 show()
             case "edit":
+                edit(user_input)
                 show()
-                edit()
             case "complete":
+                complete(user_input)
                 show()
-                complete()
             case "exit":
                 break
             case default:
