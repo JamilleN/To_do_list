a3e5c336469393ee2a236581f6bb507353d921e5
Jamille Ni <jamilletni@gmail.com>
2023-02-02 11:17:13 -0500

    Add ability to read/write the to do list in a local .txt file

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-todos = []
+f=open("user_file.txt","r")
+todos = f.readlines()
+f.close()
 
 while True:
     user_input = input ("Type add, show, edit, complete or exit\n")
@@ -6,17 +8,17 @@ while True:
 
     match user_input:
         case "add":
-            todo = input ("Enter a to-do: ")
+            todo = input ("Enter a to-do: ")+"\n"
             todos.append(todo.capitalize())
         case "show":
             print("---------------")
             for i, item in enumerate(todos):
-                print (f"{i+1} - {item}")
+                print (f"{i+1} - {item}", end="")
             print("---------------")
         case "edit":
             num = int (input("Please enter the # to edit: "))
             num = num - 1
-            new_todo = input("Please enter the new to-do: ")
+            new_todo = input("Please enter the new to-do: ")+"\n"
             todos[num] = new_todo
         case "complete":
             num = int (input("Please enter the # you have completed: "))
@@ -26,5 +28,9 @@ while True:
             break
         case default:
             print("----Invalid input, please try again.----")
-            
+    
+    f=open("user_file.txt","w")
+    f.writelines(todos)
+    f.close()
+
 print ("Thank you for using me :)")
